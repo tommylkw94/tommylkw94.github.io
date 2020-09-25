a44130de49eb2f2eb177fd628494a9fb487657e7
lkw <69156588+tommylkw94@users.noreply.github.com>
2020-09-25 19:17:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function calculate() {
     let entry = parseInt(document.getElementById("input-entry").value);
     let side = document.getElementById("long").checked ? 1 : -1;
 
-    let profit = entry + 0.2 * side * entry / lev;
-    let loss = entry - 0.1 * side * entry / lev;
+    let profit = entry + 0.1 * side * entry / lev;
+    let loss = entry - 0.05 * side * entry / lev;
 
     document.getElementById("stop-profit").innerHTML = profit;
     document.getElementById("stop-loss").innerHTML = loss;
